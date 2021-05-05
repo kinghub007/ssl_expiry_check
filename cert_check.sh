b9837e632be35e1274f3e09a714aa139e7fc64a1
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2021-05-05 13:06:47 +0300

    SSL Certificate Expiry Checks

--- cert_check.sh
+++ cert_check.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-file=./hosts.txt
+echo "Today is `date -u`"
+echo ""
+file=/home/kingshuk/hosts
 while IFS= read -r line
 do
-	echo "Today is `date -u`"
 	cert_exp=$(echo | openssl s_client -connect $line 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f 2)
 	cert_date=$(date --date="${cert_exp}" +%s)
 	today=$(date +%s)
 	date_diff=$((($cert_date - $today)/86400))
-	echo "Certificate for $line will expire in $date_diff days, on `echo -n | openssl s_client -connect $line 2>/dev/null | openssl x509 -noout -enddate | sed "s/.*=\(.*\)/\1/"`"
+	echo "Certificate for \033[1m$line\033[0m will expire in \033[1m$date_diff\033[0m days, on `echo -n | openssl s_client -connect $line 2>/dev/null | openssl x509 -noout -enddate | sed "s/.*=\(.*\)/\1/"`"
 	echo ""
 done < "$file"
